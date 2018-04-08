cdb625ad2a547a659329bfe6099790641b619508
patrick-blockseer <patrick@blockseer.com>
2018-04-07 17:22:46 -0700

    rename and better packaging

---
+++ test/dsnp-test.js
@@ -0,0 +1,11 @@
+/* eslint-env mocha */
+/* eslint prefer-arrow-callback: "off" */
+
+'use strict';
+const assert = require('assert');
+
+describe('Dsnp', function () {
+  it('dummy', async () =>{
+    assert.equal(1,1);
+  });
+});
